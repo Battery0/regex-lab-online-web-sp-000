d3c6ce754204e528f80fc0cf8ba64c053b097d27
Ian King <kickthechair@gmail.com>
2021-05-19 16:23:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,13 +13,20 @@ end
 
 
 
+
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/\b+un\w+ing/)
 end
 
+
+
+
 def words_five_letters_long(text)
   text.scan(/\b\w{5}\b/)
 end
+
+
+
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/\A[A-Z].+[.!]\z/)
@@ -33,6 +40,8 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   text.match(/\A[A-Z].+[.!]\z/) ? true : false
 end
+
+
 
 
 def valid_phone_number?(phone)
